b86cde1b4996daee78147491207a18ebeee9314e
Shozab Khan <khan.shozan.93@gmail.com>
2024-01-01 20:41:15 +0500

    first commit

---
+++ src/pages/home/index.js
@@ -0,0 +1,24 @@
+// ** MUI Imports
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
+
+const Home = () => {
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader title='Kick start your project 🚀'></CardHeader>
+          <CardContent>
+            <Typography sx={{ mb: 2 }}>All the best for your new project.</Typography>
+            <Typography>Hi This is a template for cloudflare test</Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Home
